0571cc4b3ea1e8031a3b3cfc14fadea12fc5ec5f
Amar Ganesh Pandian <amarganesh.pandian@gmail.com>
2025-04-23 02:25:31 +0530

    Update ALU.py

--- ALU.py
+++ ALU.py
@@ -4,28 +4,30 @@ carry_flag = 0
 parity_flag = 0
 zero_flag = 0
 memory = [0] * 1000000
-operand1 = 0
-operand2 = 0
-registers = [0,0,0,0,0,0,0,0]
+operand1 = None
+operand2 = None
 
-instruct_1 = input("Enter instruction")
-instruct_2 = input("Enter instruction")
-instruct_3 = input("Enter instruction")
+instruct_1 = input("Enter instruction:\n")
+instruct_2 = input("Enter instruction:\n")
+instruct_3 = input("Enter instruction:\n")
 
 if instruct_1 == "LOAD R1":
-    operand1 = int(input("Enter operand 1:"))
+    operand1 = int(input("Enter operand 1:\n"))
 elif instruct_1 == "STORE R1 MEM":
-    operand1 = int(input("Enter operand 1:"))
-    memory.append(operand1)
+    operand1 = int(input("Enter operand 1:\n"))
+    memory[0] = operand1
 
 if instruct_2 == "LOAD R2":
-    operand2 = int(input("Enter operand 2:"))
+    operand2 = int(input("Enter operand 2:\n"))
 elif instruct_2 == "STORE R2 MEM":
-    operand2 = int(input("Enter operand 2:"))
-    memory.append(operand2)
-
+    operand2 = int(input("Enter operand 2:\n"))
+    memory[1] = operand2
+if operand1 == None or operand2 == None:
+    print("One of the operand is not loaded")
+    exit()
+    
 if instruct_3 == "ADD":
-    operand1 = operand1 + operand2
+    operation = operand1 + operand2
     memory[0] = operand1
     memory[1] = operand2
     memory[2] = operation
@@ -33,7 +35,7 @@ if instruct_3 == "ADD":
     print(operand1)
 
 if instruct_3 == "SUB":
-    operand1 = operand1 - operand2
+    operation = operand1 - operand2
     memory[0] = operand1
     memory[1] = operand2
     memory[2] = operation    
@@ -41,7 +43,7 @@ if instruct_3 == "SUB":
     print(operand1)
 
 if instruct_3 == "AND":
-    operand1 = operand1 and operand2
+    operation = operand1 and operand2
     memory[0] = operand1
     memory[1] = operand2
     memory[2] = operation
@@ -49,7 +51,7 @@ if instruct_3 == "AND":
     print(operand1)
 
 if instruct_3 == "OR":
-    operand1 = operand1 or operand2
+    operation= operand1 or operand2
     memory[0] = operand1
     memory[1] = operand2
     memory[2] = operation
@@ -57,17 +59,27 @@ if instruct_3 == "OR":
     print(operand1)
 
 if instruct_3 == "XOR":
-    operand1 = operand1 ^ operand2
+    operation = operand1 ^ operand2
     memory[0] = operand1
     memory[1] = operand2
     memory[2] = operation
     print("Result stored inside memory at 0 address")
     print(operand1)
 
-# Flag calculations
+
 zero_flag = 1 if operand1 == 0 else 0
 flag = 1 if operand1 != 0 else 0
 carry_flag = 1 if operand1 >= 10 else 0
 parity_flag = 1 if operand1 % 2 == 0 else 0
 
-print(memory[0])
+print("\nFlag status")
+print("Carry Flag:", carry_flag)
+print("Zero Flag:", zero_flag)
+print("Parity Flag:", parity_flag)
+print("General Flag:", flag)
+    
+print("\nContents in memory")
+print("Address 0:", memory[0])
+print("Adress 1:", memory[1])
+print("Address 2:",memory[2])
+
